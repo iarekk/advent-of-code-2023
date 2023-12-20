3e70fc956de1a49cecde63e6c82a67329065e8f6
Iarek Kovtunenko <iarek.kovtunenko@gmail.com>
2023-12-20 19:48:31 +0000

    part 1 done

--- day6/lib/day6.ex
+++ day6/lib/day6.ex
@@ -25,4 +25,28 @@ defmodule Day6 do
 
     List.zip([times, distances]) |> IO.inspect(label: "races")
   end
+
+  def solve(file_path) do
+    race_list = get_input(file_path)
+
+    ways_to_beat_the_record = race_list |> Enum.map(&find_ways_to_beat/1)
+
+    Enum.product(ways_to_beat_the_record)
+  end
+
+  def find_ways_to_beat({record_time, distance_to_beat}) do
+    1..(record_time - 1)
+    |> Enum.count(fn time_held -> distance_attained(record_time, time_held) > distance_to_beat end)
+  end
+
+  def distance_attained(time_to_beat, time_held) do
+    time_moving = time_to_beat - time_held
+
+    # increased linearly by 1 mm/s
+    speed = time_held
+
+    distance = speed * time_moving
+
+    distance
+  end
 end
