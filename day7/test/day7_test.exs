8c08106bd96c517eee277de6213f1b889740425d
Iarek Kovtunenko <iarek.kovtunenko@gmail.com>
2023-12-20 23:15:33 +0000

    joker unit test

--- day7/test/day7_test.exs
+++ day7/test/day7_test.exs
@@ -8,6 +8,7 @@ defmodule Day7Test do
     assert Day7.compare_cards("2", "3") == :lt
     assert Day7.compare_cards("A", "2") == :gt
     assert Day7.compare_cards("A", "A") == :eq
+    assert Day7.compare_cards("J", "2") == :lt
   end
 
   test "get hand type" do
