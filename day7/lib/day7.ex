0eb5d17bf4323c60ac11885c7cb642e470713a29
Iarek Kovtunenko <iarek.kovtunenko@gmail.com>
2023-12-20 22:46:04 +0000

    card comparison logic

--- day7/lib/day7.ex
+++ day7/lib/day7.ex
@@ -28,11 +28,83 @@ defmodule Day7 do
   :lt
   """
   def compare_cards(a, b) do
-    compare(@cards_positions[a], @cards_positions[b])
+    compare_ranks(@cards_positions[a], @cards_positions[b])
   end
 
-  defp compare(a, b) when a == b, do: :eq
+  @doc """
+  Compare hands, first by rank, and then 'alphabetically'
+
+  ## Examples
+
+  iex> Day7.compare_hands("AAAAA", "KKKKK")
+  :gt
+  iex> Day7.compare_hands("AAAA8", "KKKKK")
+  :lt
+  """
+  def compare_hands(hand1, hand2) do
+    type1 = get_hand_type(hand1)
+    type2 = get_hand_type(hand2)
+
+    type_comparison = compare_ranks(@hand_types[type1], @hand_types[type2])
+
+    # not implemented - comparison by cards in hand for equal rank
+
+    case type_comparison do
+      :eq -> alpha_comparison(String.graphemes(hand1), String.graphemes(hand2))
+      _ -> type_comparison
+    end
+  end
+
+  @doc """
+  Compares the hands 'alphabetically', but using card ranks and not alphabet ordering
+
+  ## Examples
+
+  iex> Day7.alpha_comparison(["A","A"], ["A","K"])
+  :gt
+  iex> Day7.alpha_comparison(["A","Q"], ["A","K"])
+  :lt
+  iex(21)> Day7.alpha_comparison(["A","K"], ["A","K"])
+  :eq
+  """
+  def alpha_comparison([], []), do: :eq
+
+  def alpha_comparison([c1 | rem1], [c2 | rem2]) when c1 == c2, do: alpha_comparison(rem1, rem2)
+
+  def alpha_comparison([c1 | _], [c2 | _]), do: compare_cards(c1, c2)
+
+  def get_hand_type(hand_str) do
+    cards = String.graphemes(hand_str)
+
+    chunks =
+      cards
+      |> Enum.sort()
+      |> Enum.chunk_by(& &1)
+      |> Enum.sort_by(&length/1, :desc)
+
+    # |> IO.inspect(label: "chunks")
+
+    classify_hand(chunks)
+    # AAAAA - AAAAA - all same
+    # AA8AA - AAAA 8 - four of a kind
+    # 23332 - 333 22 - full house (3+2)
+    # TTT98 - TTT 9 8 - three of kind
+    # 23432 - 4 33 22
+    # A23A4 - AA 4 3 2
+    # 23456 - 6 5 4 3 2
+  end
+
+  defp classify_hand([_]), do: :five_of_kind
+  defp classify_hand([[_], [_], [_], [_], [_]]), do: :high_card
+  defp classify_hand([l1, _]) when length(l1) == 4, do: :four_of_kind
+  defp classify_hand([_, _]), do: :full_house
+  defp classify_hand([l1, _, _]) when length(l1) == 3, do: :three_of_kind
+  defp classify_hand([_, _, _]), do: :two_pairs
+  defp classify_hand([_, _, _, _]), do: :one_pair
+  defp classify_hand(_), do: :error
+
+  defp compare_ranks(a, b) when a == b, do: :eq
   # lower number means 'higher' rank and vice versa
-  defp compare(a, b) when a < b, do: :gt
-  defp compare(a, b) when a > b, do: :lt
+  defp compare_ranks(a, b) when a < b, do: :gt
+  defp compare_ranks(a, b) when a > b, do: :lt
 end
--- day7/test/day7_test.exs
+++ day7/test/day7_test.exs
@@ -9,4 +9,14 @@ defmodule Day7Test do
     assert Day7.compare_cards("A", "2") == :gt
     assert Day7.compare_cards("A", "A") == :eq
   end
+
+  test "get hand type" do
+    assert Day7.get_hand_type("AAAAA") == :five_of_kind
+    assert Day7.get_hand_type("AA8AA") == :four_of_kind
+    assert Day7.get_hand_type("23332") == :full_house
+    assert Day7.get_hand_type("TTT98") == :three_of_kind
+    assert Day7.get_hand_type("23432") == :two_pairs
+    assert Day7.get_hand_type("A23A4") == :one_pair
+    assert Day7.get_hand_type("23456") == :high_card
+  end
 end
