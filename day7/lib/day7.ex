52282dbf3c7ba547edbb4f2ba2ff13b473d21bfb
Iarek Kovtunenko <iarek.kovtunenko@gmail.com>
2023-12-20 22:08:57 +0000

    card comparison and tests

--- day7/lib/day7.ex
+++ day7/lib/day7.ex
@@ -5,29 +5,34 @@ defmodule Day7 do
 
   @cards ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"]
 
-  def cards_positions(),
-    do: Enum.with_index(@cards) |> Map.new() |> IO.inspect(label: "card positions invoked")
+  @cards_positions Enum.with_index(@cards) |> Map.new()
 
+  @hand_types %{
+    :five_of_kind => 0,
+    :four_of_kind => 1,
+    :full_house => 2,
+    :three_of_kind => 3,
+    :two_pairs => 4,
+    :one_pair => 5,
+    :high_card => 6
+  }
+
+  @doc """
+  Compares two cards.
+
+  ## Examples
+  iex> Day7.compare_cards("A", "Q")
+  :gt
+
+  iex> Day7.compare_cards("J", "Q")
+  :lt
+  """
   def compare_cards(a, b) do
-    comp_map = cards_positions()
-
-    compare(comp_map[a], comp_map[b])
+    compare(@cards_positions[a], @cards_positions[b])
   end
 
   defp compare(a, b) when a == b, do: :eq
+  # lower number means 'higher' rank and vice versa
   defp compare(a, b) when a < b, do: :gt
   defp compare(a, b) when a > b, do: :lt
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Day7.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
