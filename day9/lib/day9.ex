7183e7c252354180ab16ce2a84144f90cc8edf6e
Iarek Kovtunenko <iarek.kovtunenko@gmail.com>
2023-12-25 17:08:57 +0000

    Day 9 part 1 - extrapolating next value (answer 1934898178)

---
+++ day9/lib/day9.ex
@@ -0,0 +1,65 @@
+defmodule Day9 do
+  @moduledoc """
+  Documentation for `Day9`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Day9.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+  def solve(file_path) do
+    input_lines = read_file(file_path)
+
+    input_lines |> Enum.map(&find_next_value/1) |> Enum.sum()
+  end
+
+  def find_next_value(number_list) do
+    derivatives = find_derivatives(number_list, [])
+
+    find_next_value(0, derivatives)
+  end
+
+  def find_next_value(delta, []) do
+    delta
+  end
+
+  def find_next_value(delta, [number_list | rest_of_lists]) do
+    next_val = List.last(number_list) + delta
+    find_next_value(next_val, rest_of_lists)
+  end
+
+  def find_derivatives(number_list, acc) do
+    if(number_list |> Enum.all?(&(&1 == 0))) do
+      acc
+    else
+      derivative_list = derive(number_list)
+      find_derivatives(derivative_list, [number_list | acc])
+    end
+  end
+
+  def derive([a, b]), do: [b - a]
+
+  def derive([a, b | rest]) do
+    [b - a | derive([b | rest])]
+  end
+
+  def read_file(file_path) do
+    File.stream!(file_path)
+    |> Stream.map(&process_line/1)
+    |> Enum.to_list()
+    |> IO.inspect(label: "input")
+  end
+
+  def process_line(str) do
+    str |> String.trim() |> String.split() |> Enum.map(&String.to_integer/1)
+  end
+end
