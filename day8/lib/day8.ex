deb4de70138a17691c5be40466e0d85be169c21d
Iarek Kovtunenko <16271112+iarekk@users.noreply.github.com>
2023-12-25 16:36:08 +0000

    updated to using the LCM on loop lengths

--- day8/lib/day8.ex
+++ day8/lib/day8.ex
@@ -36,8 +36,22 @@ defmodule Day8 do
     start_nodes =
       tree |> Map.keys() |> Enum.filter(&is_start_node?/1) |> IO.inspect(label: "start nodes")
 
-    commands |> Enum.reduce_while({start_nodes, 0, tree}, &perform_step/2)
+    # commands |> Enum.reduce_while({start_nodes, 0, tree}, &perform_step/2)
+
+    lengths =
+      start_nodes
+      |> Enum.map(fn node -> find_length_for_start_node(commands, node, tree) end)
+      |> IO.inspect(label: "lengths")
+
+    # solution owed to this thread: https://elixirforum.com/t/advent-of-code-2023-day-8/60244
+
+    lowest_common_multiplier(lengths)
   end
+
+  def lowest_common_multiplier([a]), do: a
+
+  def lowest_common_multiplier([a, b | rest]),
+    do: lowest_common_multiplier([Math.lcm(a, b) | rest])
 
   def find_length_for_start_node(command_stream, start_node, tree) do
     command_stream |> Enum.reduce_while({start_node, 0, tree}, &perform_step/2)
@@ -57,7 +71,7 @@ defmodule Day8 do
 
   #  def perform_step(_, {node, step_count, _}) when is_end_step(node), do: {:halt, step_count}
 
-  def perform_step(command, {node_keys, step_count, tree}) do
+  def perform_step_batch(command, {node_keys, step_count, tree}) do
     if(Enum.all?(node_keys, &is_end_node?/1)) do
       {:halt, step_count}
     else
@@ -71,6 +85,23 @@ defmodule Day8 do
       end
 
       {:cont, {next_node_keys, step_count + 1, tree}}
+    end
+  end
+
+  def perform_step(command, {node_key, step_count, tree}) do
+    if(is_end_node?(node_key)) do
+      {:halt, step_count}
+    else
+      next_node_key =
+        get_next_node(command, tree[node_key])
+
+      if(rem(step_count, 1000) == 0) do
+        IO.puts(
+          "steps: #{step_count} node: #{node_key} command: #{command} next: #{next_node_key}"
+        )
+      end
+
+      {:cont, {next_node_key, step_count + 1, tree}}
     end
   end
 
